f0b7b2119ea334da9faec4498417af8e55a6fce1
Kerim Guseynov <guseynovkerim@gmail.com>
2020-06-23 01:43:51 +0300

    Font2 patch.

--- config.h
+++ config.h
@@ -5,7 +5,14 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Monospace:size=10:antialias=true:autohint=true"; 
+//static char *font = "Terminus (TTF):pixelsize=22:antialias=true:autohint=true";
+
+/* Spare fonts */
+static char *font2[] = {
+	"Monospace:antialias=true:autohint=true",
+	"Symbola:antialias=true:autohint=true"
+};
 static int borderpx = 2;
 
 /*
